368f8375795fe95b5a0da22f3329e05ea3ba84c7
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-11-23 15:21:24 -0800

    Handle state in own class

--- plugin/src/main/java/tech/strateim/barium/Master/Remote.java
+++ plugin/src/main/java/tech/strateim/barium/Master/Remote.java
@@ -1,20 +1,14 @@
 package tech.strateim.barium.Master;
 
-import init.Os;
-import init.Protocol;
-import init.Register;
-import init.RegistrationResponse;
+import com.github.retrooper.packetevents.PacketEventsAPI;
 import org.bukkit.Server;
-import tech.strateim.barium.Master.Enum.Status;
-import tech.strateim.barium.Master.State.PacketHandler;
-import tech.strateim.barium.Master.Utilities.PacketEventsConversion;
+import tech.strateim.barium.Master.Packet.PacketHandler;
+import tech.strateim.barium.Master.State.StateHandler;
 
 import javax.annotation.Nullable;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
@@ -26,15 +20,14 @@ public class Remote {
     private OutputStream SocketOutput;
     private InputStream SocketReceive;
     public final ExecutorService ExecutorService = Executors.newFixedThreadPool(8);
-    public Status State = Status.Init;
-
     private PacketHandler PacketHandler;
+    private StateHandler StateHandler;
 
     public Remote(Logger log) {
         Log = log;
     }
 
-    public @Nullable PacketHandler Start(String governorAddress, int governorPort, Server localServer) {
+    public @Nullable PacketHandler Start(String governorAddress, int governorPort, Server localServer, PacketEventsAPI<?> packetEvents) {
         while (true) {
             try {
                 Socket = new Socket(governorAddress, governorPort);
@@ -54,7 +47,6 @@ public class Remote {
             SocketOutput = Socket.getOutputStream();
         } catch (Exception ex) {
             Log.log(Level.SEVERE, "Failed to get sender socket stream to governor server");
-            State = Status.Crash;
 
             return null;
         }
@@ -63,15 +55,15 @@ public class Remote {
             SocketReceive = Socket.getInputStream();
         } catch (Exception ex) {
             Log.log(Level.SEVERE, "Failed to get receiver socket stream to governor server");
-            State = Status.Crash;
 
             return null;
         }
 
         PacketHandler = new PacketHandler(Log, SocketOutput, SocketReceive);
+        StateHandler = new StateHandler(PacketHandler, SocketOutput, SocketReceive, Log);
 
-        ExecutorService.execute(this::StartGovernorReceiver);
-        ExecutorService.execute(() -> InitRegister(localServer, packetEvents));
+        ExecutorService.execute(StateHandler::StartReceiver);
+        ExecutorService.execute(() -> StateHandler.RegistrationHandler.InitRegister(localServer, packetEvents));
 
         return PacketHandler;
     }
---
+++ plugin/src/main/java/tech/strateim/barium/Master/State/StateHandler.java
@@ -0,0 +1,58 @@
+package tech.strateim.barium.Master.State;
+
+import tech.strateim.barium.Master.Enum.Status;
+import tech.strateim.barium.Master.Packet.Packet;
+import tech.strateim.barium.Master.Packet.PacketHandler;
+import tech.strateim.barium.Master.State.Registration.RegistrationHandler;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+public class StateHandler {
+    public Status State = Status.Init;
+    private final PacketHandler PacketHandler;
+    private final Logger Log;
+    private final OutputStream SocketOutput;
+    private final InputStream SocketReceive;
+    public final RegistrationHandler RegistrationHandler;
+
+    public StateHandler(PacketHandler packetHandler, OutputStream socketOutput, InputStream socketReceive, Logger log) {
+        PacketHandler = packetHandler;
+        SocketOutput = socketOutput;
+        SocketReceive = socketReceive;
+        Log = log;
+
+        RegistrationHandler = new RegistrationHandler(packetHandler, this, log);
+    }
+
+    public void StartReceiver() {
+        synchronized (SocketReceive) {
+            while(true) {
+                try {
+                    SocketReceive.wait(1);
+
+                    Packet packet = PacketHandler.ReceivePacketBlocking();
+
+                    if (packet == null) {
+                        continue;
+                    }
+
+                    Log.warning("Packet received ID: " + packet.id());
+
+                    switch (packet.id()) {
+                        case 1 -> RegistrationHandler.HandleResponse(packet);
+                        default -> {
+                            Log.severe("UNKNOWN Packet Id received: " + packet.id());
+                            break;
+                        }
+                    }
+
+                } catch (Exception ex) {
+                    Log.severe("Failed to receive data from remote governor");
+                    Log.severe(ex.toString());
+                }
+            }
+        }
+    }
+}
