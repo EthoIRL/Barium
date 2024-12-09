a050000a24797830c9705799a154530f6c426489
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-12-08 22:16:29 -0800

    Pass logger & packet handler through abstract state

--- plugin/src/main/java/tech/strateim/barium/Master/State/AbstractState.java
+++ plugin/src/main/java/tech/strateim/barium/Master/State/AbstractState.java
@@ -1,13 +1,21 @@
 package tech.strateim.barium.Master.State;
 
 import tech.strateim.barium.Master.Packet.Packet;
+import tech.strateim.barium.Master.Packet.PacketHandler;
+
+import java.util.logging.Logger;
 
 public abstract class AbstractState {
     protected int id = -1;
+    protected PacketHandler packetHandler;
+
+    protected Logger log;
 
     public abstract void HandleResponse(Packet packet) throws Exception;
 
-    public AbstractState(int id) {
+    public AbstractState(int id, PacketHandler packetHandler, Logger log) {
         this.id = id;
+        this.packetHandler = packetHandler;
+        this.log = log;
     }
 }
