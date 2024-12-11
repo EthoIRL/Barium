8d7ba90cca8423984ca8a0746ecab78e8b2cd6d1
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-12-10 23:02:04 -0800

    Move rust generated protobufs into separate modules

--- governor/src/proto/mod.rs
+++ governor/src/proto/mod.rs
@@ -1,2 +1,11 @@
-include!(concat!(env!("OUT_DIR"), "/init.rs"));
-include!(concat!(env!("OUT_DIR"), "/anticheat.rs"));
+pub mod generic {
+    include!(concat!(env!("OUT_DIR"), "/generic.rs"));
+}
+
+pub mod anticheat {
+    include!(concat!(env!("OUT_DIR"), "/anticheat.rs"));
+}
+
+pub mod server {
+    include!(concat!(env!("OUT_DIR"), "/server.rs"));
+}
