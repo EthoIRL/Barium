cac7fdee80f3fc21b4f94db3f77fbf5a625e6ba4
Etho <58352929+EthoIRL@users.noreply.github.com>
2024-12-11 22:06:20 -0800

    Handle error during listener startup

--- governor/src/main.rs
+++ governor/src/main.rs
@@ -1,8 +1,7 @@
 use std::thread;
 use std::time::Duration;
 
-use crate::server::client;
-use crate::server::client::Client;
+use crate::server::{client, node};
 
 mod server;
 mod proto;
@@ -10,14 +9,22 @@ mod plugin;
 pub mod packet;
 
 pub const API_VERSION: i32 = 0;
-
-static mut CLIENTS: Vec<Client> = Vec::new();
+const CLIENT_ADDRESS: (&str, u16) = ("127.0.0.1", 3238);
+const NODE_ADDRESS: (&str, u16) = ("127.0.0.1", 3349);
 
 fn main() {
-    let address = ("127.0.0.1", 3238);
-    println!("[GOV] Starting proxy server, {:?}", &address);
+    println!("[GOV] Starting proxy server, {:?}", &CLIENT_ADDRESS);
+    println!("[GOV] Starting node server, {:?}", &NODE_ADDRESS);
 
-    client::start_plugin_server(address).unwrap();
+    if let Err(err) = node::start_node_server(NODE_ADDRESS) {
+        eprintln!("[GOV] Failed to start node server, ({})", err);
+        return;
+    }
+
+    if let Err(err) = client::start_plugin_server(CLIENT_ADDRESS) {
+        eprintln!("[GOV] Failed to start client server, ({})", err);
+        return;
+    }
 
     loop {
         thread::sleep(Duration::from_secs(1));
