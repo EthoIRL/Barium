dc31ec373124a74ceb146700ca5cde33e5dcab40
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-01-06 15:13:54 -0800

    Convert registration into generic packet form

--- governor/src/plugin/registration.rs
+++ governor/src/plugin/registration.rs
@@ -1,4 +1,7 @@
-use std::io;
+use std::{fmt, io};
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::io::ErrorKind;
 
 use prost::Message;
 use uuid::Uuid;
@@ -7,12 +10,37 @@ use crate::API_VERSION;
 use crate::proto::server::server_registration::{Register, Response};
 use crate::server::client::{Client, Status};
 use crate::packet;
+use crate::packet::{GenericHandler, GenericPacket};
+use crate::proto::generic::DisconnectReason;
+use crate::server::client;
 
 #[derive(Debug)]
 pub enum RegistrationError {
-    MismatchVersion,
+    MismatchVersion { supplied_version: i32 },
     BadResponse,
     Unknown,
+}
+
+impl Display for RegistrationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            RegistrationError::MismatchVersion { supplied_version} => {
+                write!(f, "version supplied from server client does not match governor version (Supplied: {}, Current: {})", supplied_version, API_VERSION)
+            },
+            RegistrationError::BadResponse => {
+                write!(f, "error occurred when sending registration response")
+            },
+            RegistrationError::Unknown => {
+                write!(f, "an unknown error occurred when handling registration")
+            }
+        }
+    }
+}
+
+impl Into<Box<dyn Error>> for RegistrationError {
+    fn into(self) -> Box<dyn Error> {
+        io::Error::new(ErrorKind::Other, self.to_string()).into()
+    }
 }
 
 pub fn verify_registration(packet: Vec<u8>) -> Result<Register, RegistrationError> {
@@ -24,31 +52,12 @@ pub fn verify_registration(packet: Vec<u8>) -> Result<Register, RegistrationErro
     };
 
     if register.plugin_version != API_VERSION {
-        return Err(RegistrationError::MismatchVersion);
+        return Err(RegistrationError::MismatchVersion {
+            supplied_version: register.plugin_version
+        });
     }
 
     Ok(register)
-}
-
-pub fn handle_registration(client: &mut Client, packet: Vec<u8>) -> Result<(), RegistrationError> {
-    let result = verify_registration(packet);
-
-    let key  = match handle_response(client, result.is_ok()) {
-        Ok(key) => key,
-        Err(_) => return Err(RegistrationError::BadResponse)
-    };
-
-    return match result {
-        Ok(register) => {
-            client.state = Some(register);
-            client.status = Status::Registered;
-            client.key = Some(key);
-
-            println!("{:#?}", register);
-            Ok(())
-        },
-        Err(err) => Err(err)
-    }
 }
 
 fn handle_response(client: &mut Client, authenticated: bool) -> Result<Uuid, io::Error> {
@@ -63,4 +72,38 @@ fn handle_response(client: &mut Client, authenticated: bool) -> Result<Uuid, io:
     packet::send_packet(response, 1, &mut client.stream)?;
 
     Ok(key)
+}
+
+pub struct ClientRegistration;
+
+impl GenericHandler<Client, GenericPacket> for ClientRegistration {
+    fn handle(client: &mut Client, packet: GenericPacket) -> Result<(), Box<dyn Error>> {
+        let result = verify_registration(packet.data);
+
+        let key  = match handle_response(client, result.is_ok()) {
+            Ok(key) => key,
+            Err(_) => return Err(RegistrationError::BadResponse.into())
+        };
+
+        return match result {
+            Ok(register) => {
+                client.state = Some(register);
+                client.status = Status::Registered;
+                client.key = Some(key);
+
+                println!("{:#?}", register);
+                Ok(())
+            },
+            Err(err) => {
+                println!("Failed to authenticate client, (Reason: {:#?})", err);
+
+                client::disconnect_client(client, match err {
+                    RegistrationError::MismatchVersion {supplied_version} => DisconnectReason::MismatchVersion,
+                    _ => DisconnectReason::Unknown
+                });
+
+                Err(err.into())
+            }
+        }
+    }
 }
