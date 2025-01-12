8b305376561a9fb2c91e7a120e33ec1020cf36e8
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-01-11 18:25:45 -0800

    Implement registration for nodes

--- governor/src/anticheat/registration.rs
+++ governor/src/anticheat/registration.rs
@@ -1,11 +1,40 @@
-use std::io::Error;
+use crate::{API_VERSION, NODE_KEY, packet};
+use crate::error::RegistrationError;
 use crate::packet::{GenericHandler, GenericPacket};
+use crate::proto::anticheat::node_registration;
+use crate::proto::generic::DisconnectReason;
+use crate::server::node;
 use crate::server::node::Node;
 
 pub struct NodeRegistar;
 
 impl GenericHandler<Node, GenericPacket> for NodeRegistar {
     fn handle(node: &mut Node, packet: GenericPacket) -> Result<(), Box<dyn std::error::Error>> {
-        todo!()
+        let registration_packet = packet.decode::<node_registration::Register>()?;
+
+        if registration_packet.node_version != API_VERSION {
+            node::disconnect_node(node, DisconnectReason::Unknown);
+            return Err(RegistrationError::MismatchVersion {
+                supplied_version: registration_packet.node_version
+            }.into());
+        }
+
+        if registration_packet.shared_key != NODE_KEY {
+            node::disconnect_node(node, DisconnectReason::Unknown);
+            return Err(RegistrationError::BadResponse.into());
+        }
+
+        node.resources = registration_packet.resources;
+
+        let response = node_registration::Response {
+            succeeded: true,
+        };
+
+        if let Err(err) = packet::send_packet(response, 1, &mut node.stream) {
+            node::disconnect_node(node, DisconnectReason::Unknown);
+            return Err(err.into());
+        };
+
+        Ok(())
     }
 }
