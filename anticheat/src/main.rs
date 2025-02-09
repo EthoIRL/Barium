5a6d68559331f1c8277250a98cef48407605db3b
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-02-09 14:29:14 -0800

    Improve logging during startup of node

--- anticheat/src/main.rs
+++ anticheat/src/main.rs
@@ -15,11 +15,25 @@ pub const API_VERSION: i32 = 0;
 const NODE_GOVERNOR: (&str, u16) = ("127.0.0.1", 3349);
 
 fn main() {
-    println!("Hello, world!");
+    println!("[NODE] Connecting to governor... {:?}", NODE_GOVERNOR);
+    
+    let mut stream = match TcpStream::connect(NODE_GOVERNOR) {
+        Ok(stream) => stream,
+        Err(err) => {
+            println!("[NODE] Failed to connect to governor server... ({})", err);
+            return;
+        }
+    };
 
-    let mut stream = TcpStream::connect(NODE_GOVERNOR).unwrap();
-    auth::authed_connection(&mut stream, "shared key").unwrap();
-    connection::handle_registration(&mut stream).unwrap();
+    if let Err(err) = auth::authed_connection(&mut stream, "shared key") {
+        println!("[NODE] Failed to send authed registration packet... ({})", err);
+        return;
+    }
+
+    if let Err(err) = connection::handle_registration(&mut stream) {
+        println!("[NODE] {}", err);
+        return;
+    };
 
     let servers: Arc<RwLock<HashMap<Uuid, Arc<GameServer>>>> = Arc::new(RwLock::new(HashMap::new()));
 
