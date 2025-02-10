3f98806770142303e89278399bbdf3ce8eb0e93b
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-02-09 16:01:12 -0800

    Add basic template for handling PlayerJoin packet

--- anticheat/src/client/mod.rs
+++ anticheat/src/client/mod.rs
@@ -1,2 +1,3 @@
 pub mod game;
-mod player;
+pub mod player;
+mod packets;
--- anticheat/src/client/player.rs
+++ anticheat/src/client/player.rs
@@ -1,5 +1,17 @@
+use crate::client::game::GameServer;
+use crate::packet::{GenericHandler, GenericPacket};
 
 pub struct Player {
     uuid: String,
     name: String
+}
+
+impl GenericHandler<GameServer, GenericPacket> for Player {
+    fn handle(game_server: &mut GameServer, packet: GenericPacket) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+
+    fn id() -> u16 {
+        0
+    }
 }
