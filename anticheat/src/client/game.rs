6f5582f2e9388aeccbbaf85cf986822483b94f44
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-02-10 16:58:48 -0800

    Players should be wrapped in a rwlock

--- anticheat/src/client/game.rs
+++ anticheat/src/client/game.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
+use std::sync::RwLock;
 use crate::client::player::Player;
 use crate::proto::generic::ServerInfo;
 
 pub struct GameServer {
     pub info: ServerInfo,
-    pub players: HashMap<String, Player>,
+    pub players: RwLock<HashMap<String, Player>>,
 }
--- anticheat/src/node/connection.rs
+++ anticheat/src/node/connection.rs
@@ -71,7 +71,7 @@ pub fn start_client_server(governor_address: (&str, u16), stream: &mut TcpStream
 
         let mut game_server = Arc::new(GameServer {
             info: negotiation.server_info.unwrap(),
-            players: HashMap::new()
+            players: RwLock::new(HashMap::new())
         });
 
         let game_servers = game_servers.clone();
