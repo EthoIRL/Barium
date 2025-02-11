d848b6dbd6724706f76104a86359207c49651e32
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-02-10 17:01:12 -0800

    Implement basic player joining mechanism

--- anticheat/src/client/player.rs
+++ anticheat/src/client/player.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 use crate::client::game::GameServer;
 use crate::packet::{GenericHandler, GenericPacket};
+use crate::proto::game::PxPlayerJoin;
 
 pub struct Player {
     uuid: String,
@@ -9,7 +10,29 @@ pub struct Player {
 
 impl GenericHandler<Arc<GameServer>, GenericPacket> for Player {
     fn handle(game_server: &mut Arc<GameServer>, packet: GenericPacket) -> Result<(), Box<dyn std::error::Error>> {
-        todo!()
+        let player_packet = packet.decode::<PxPlayerJoin>()?;
+
+        if player_packet.uuid.is_empty() {
+            return Err("Player's uuid cannot be empty... Malformed player join packet.".into());
+        }
+
+        let player = Player {
+            uuid: player_packet.uuid.clone(),
+            name: player_packet.name
+        };
+
+        match game_server.players.write() {
+            Ok(mut players) => {
+                players.insert(player_packet.uuid, player);
+
+                // println!("[NODE] [SERVER] New player has been registered to server ({}, {})")
+            },
+            Err(err) => {
+                return Err(err.to_string().into());
+            }
+        }
+
+        Ok(())
     }
 
     fn id() -> u16 {
