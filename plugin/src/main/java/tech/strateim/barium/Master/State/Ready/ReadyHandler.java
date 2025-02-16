b1a8c77531cee3c20f58d77371f99b60db07824e
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-02-16 12:43:29 -0800

    Register already connected players during runtime

--- plugin/src/main/java/tech/strateim/barium/Master/State/Ready/ReadyHandler.java
+++ plugin/src/main/java/tech/strateim/barium/Master/State/Ready/ReadyHandler.java
@@ -1,45 +1,47 @@
 package tech.strateim.barium.Master.State.Ready;
 
-import generic.Protocol;
-import server.ProxyMessage;
-import server.ServerRegistration;
+import com.github.retrooper.packetevents.protocol.player.User;
+import network.Px_PlayerJoin;
+import tech.strateim.barium.Game.ServerState;
 import tech.strateim.barium.Master.Enum.Status;
 import tech.strateim.barium.Master.Packet.Packet;
 import tech.strateim.barium.Master.Packet.PacketHandler;
 import tech.strateim.barium.Master.State.AbstractState;
 import tech.strateim.barium.Master.State.StateHandler;
 
+import java.util.HashMap;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class ReadyHandler extends AbstractState {
 
     private final StateHandler stateHandler;
+    private final ServerState serverState;
 
-    public ReadyHandler(int id, PacketHandler packetHandler, Logger log, StateHandler stateHandler) {
+    public ReadyHandler(int id, PacketHandler packetHandler, Logger log, ServerState serverState, StateHandler stateHandler) {
         super(id, packetHandler, log);
         this.stateHandler = stateHandler;
+        this.serverState = serverState;
     }
 
     @Override
     public void HandleResponse(Packet packet) throws Exception {
+        log.warning("READY TO SEND");
         stateHandler.State = Status.Ready;
 
-        log.warning("READY TO SEND");
+        HashMap<UUID, User> users = serverState.getUsers();
 
-        while(true) {
-            Protocol protocol = Protocol.newBuilder()
-                    .setGeyser(true)
-                    .setViaBackwards(false)
-                    .setViaRewind(true)
-                    .setViaVersion(false)
+        for (UUID key: users.keySet()) {
+            User user = users.get(key);
+
+            log.warning("Registering: " + user.getName());
+
+            Px_PlayerJoin playerJoin = Px_PlayerJoin.newBuilder()
+                    .setUuid(key.toString())
+                    .setName(user.getName())
                     .build();
 
-            packetHandler.SendPacket(protocol, 200, stateHandler.State);
-
-            Thread.sleep(500);
+            packetHandler.SendPacketRetry(playerJoin, 0, stateHandler.State);
         }
-
-        // TODO: Begin sending packets to anticheat
-        // TODO: Register all connected players with anticheat
     }
 }
