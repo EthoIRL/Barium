5e56f2efd58172d694669dc965ea14dd418d5b6b
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-03-09 15:48:45 -0700

    Remove ground packet handler from hashmap

--- anticheat/src/node/connection.rs
+++ anticheat/src/node/connection.rs
@@ -12,7 +12,7 @@ use crate::packet;
 use crate::packet::{GenericHandler, GenericPacket};
 use crate::proto::anticheat::node_registration::Response;
 use crate::proto::anticheat::NodeProxyNegotiation;
-use crate::proto::game::{PxPlayerGround, PxPlayerJoin, PxPlayerLeave, PxPlayerMovement, PxPlayerRotation};
+use crate::proto::game::{PxPlayerJoin, PxPlayerLeave, PxPlayerMovement, PxPlayerRotation};
 
 pub fn handle_registration(stream: &mut TcpStream) -> Result<(), Error> {
     let mut packet_id_buffer = [0u8; 2];
@@ -48,7 +48,6 @@ pub fn start_client_server(governor_address: (&str, u16), stream: &mut TcpStream
     packet_handles.insert(PxPlayerLeave::id(), PxPlayerLeave::handle);
     packet_handles.insert(PxPlayerMovement::id(), PxPlayerMovement::handle);
     packet_handles.insert(PxPlayerRotation::id(), PxPlayerRotation::handle);
-    packet_handles.insert(PxPlayerGround::id(), PxPlayerGround::handle);
 
     let arc_packet_handles = Arc::new(packet_handles);
 
