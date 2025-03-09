211dc7b10d5bd47069cdc8657607799c23661fe3
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-03-09 15:49:32 -0700

    Update handlers to use correct positional buffers

--- anticheat/src/client/state/handlers/movement.rs
+++ anticheat/src/client/state/handlers/movement.rs
@@ -1,22 +1,22 @@
 use std::sync::Arc;
 use crate::client::state::game::GameServer;
+use crate::client::state::player::{LocationPosition, RotationPosition};
 use crate::packet::{GenericHandler, GenericPacket};
-use crate::proto::game::{PxPlayerGround, PxPlayerMovement, PxPlayerRotation};
+use crate::proto::game::{PxPlayerMovement, PxPlayerRotation};
 
 impl GenericHandler<Arc<GameServer>, GenericPacket> for PxPlayerMovement {
     fn handle(game_server: &mut Arc<GameServer>, packet: GenericPacket) -> Result<(), Box<dyn std::error::Error>> {
         let player_movement = packet.decode::<PxPlayerMovement>()?;
-        println!("[{:#?} {:#?} {:#?}]", player_movement.x, player_movement.y, player_movement.z);
-
-        if let Ok(player_list) = game_server.players.write() {
-            let (_, player) = player_list.iter().find(|(uuid, _)| uuid == &&player_movement.uuid)
+        if let Ok(mut player_list) = game_server.players.write() {
+            let (_, player) = player_list.iter_mut().find(|(uuid, _)| uuid == &&player_movement.uuid)
                 .ok_or(format!("Unknown uuid received from within sub packet, ({})", player_movement.uuid))?;
 
-            if let Ok(mut player_position) = player.position.write() {
-                player_position.x = Some(player_movement.x);
-                player_position.y = Some(player_movement.y);
-                player_position.z = Some(player_movement.z);
-            }
+            player.locational_position.push_front(LocationPosition {
+                x: player_movement.x,
+                y: player_movement.y,
+                z: player_movement.z,
+                ground: player_movement.ground
+            });
         }
 
         Ok(())
@@ -30,16 +30,14 @@ impl GenericHandler<Arc<GameServer>, GenericPacket> for PxPlayerMovement {
 impl GenericHandler<Arc<GameServer>, GenericPacket> for PxPlayerRotation {
     fn handle(game_server: &mut Arc<GameServer>, packet: GenericPacket) -> Result<(), Box<dyn std::error::Error>> {
         let player_rotation = packet.decode::<PxPlayerRotation>()?;
-        println!("[{:#?} {:#?}]", player_rotation.yaw, player_rotation.pitch);
-
-        if let Ok(player_list) = game_server.players.write() {
-            let (_, player) = player_list.iter().find(|(uuid, _)| uuid == &&player_rotation.uuid)
+        if let Ok(mut player_list) = game_server.players.write() {
+            let (_, player) = player_list.iter_mut().find(|(uuid, _)| uuid == &&player_rotation.uuid)
                 .ok_or(format!("Unknown uuid received from within sub packet, ({})", player_rotation.uuid))?;
 
-            if let Ok(mut player_position) = player.position.write() {
-                player_position.yaw = Some(player_rotation.yaw);
-                player_position.pitch = Some(player_rotation.pitch);
-            }
+            player.rotational_position.push_front(RotationPosition {
+                yaw: player_rotation.yaw,
+                pitch: player_rotation.pitch
+            })
         }
 
         Ok(())
