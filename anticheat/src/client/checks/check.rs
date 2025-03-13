f9292a5abba3cda69d3ed1734b218b9ff7369c16
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-03-12 22:48:13 -0700

    Introduce default check stucture

---
+++ anticheat/src/client/checks/check.rs
@@ -0,0 +1,30 @@
+use std::fmt::Display;
+use std::sync::Arc;
+use crate::client::state::game::GameServer;
+use crate::client::state::player::Player;
+
+pub trait GenericCheck {
+    fn handle(player: &Player, game_server: &Arc<GameServer>);
+
+    fn get_info() -> CheckInfo;
+}
+
+pub struct CheckInfo {
+    pub r#type: CheckType,
+    pub name: String,
+    pub weight: i8,
+    pub experimental: bool
+}
+
+#[derive(Debug)]
+pub enum CheckType {
+    Movement,
+    Combat,
+    Other
+}
+
+impl Display for CheckType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
