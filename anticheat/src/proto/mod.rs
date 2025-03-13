578bd0265ddd2f402b11fb4d95b84ac3105fb90a
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-03-12 22:56:26 -0700

    Add PlayerWarn proto and command mod.rs

--- anticheat/src/proto/mod.rs
+++ anticheat/src/proto/mod.rs
@@ -9,4 +9,5 @@ pub mod anticheat {
 pub mod game {
     include!(concat!(env!("OUT_DIR"), "/network.rs"));
     include!(concat!(env!("OUT_DIR"), "/movement.rs"));
+    include!(concat!(env!("OUT_DIR"), "/command.rs"));
 }
